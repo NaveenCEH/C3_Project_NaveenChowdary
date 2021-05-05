ae3975810c16600e5dee74e2e093362420bd2b04
naveenchowdary <naveenchowdary96@gmail.com>
2021-05-05 21:41:16 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -6,21 +6,61 @@ import java.time.LocalTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantTest {
-    Restaurant restaurant;
-    //REFACTOR ALL THE REPEATED LINES OF CODE
+	 Restaurant restaurant;
+	    //REFACTOR ALL THE REPEATED LINES OF CODE
 
-    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
-    @Test
-    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
-    }
+	    private void mockData()
+	    {
+	        LocalTime openingTime = LocalTime.parse("10:30:00");
+	        LocalTime closingTime = LocalTime.parse("22:00:00");
+	        restaurant =new Restaurant("Paradise Restaurant","Hyderabad",openingTime,closingTime);
+	        restaurant.addToMenu("Veg Dum Biryani",200,true);
+	        restaurant.addToMenu("Baida Curry", 156,false);
+	    }
+	    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-    @Test
-    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
+	    @Test
+	    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+	        mockData();
+	        LocalTime currentTime = LocalTime.parse("15:45:00");
+	        boolean restaurantStatus = restaurant.isRestaurantOpen(currentTime);
+	        assertTrue(restaurantStatus);
+	    }
 
-    }
+	    @Test
+	    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+	        mockData();
+	        LocalTime currentTime = LocalTime.parse("22:45:00");
+	        boolean restaurantStatus = restaurant.isRestaurantOpen(currentTime);
+	        assertFalse(restaurantStatus);
+	    }
 
+	    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+	    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	    @Test
+	    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+	        mockData();
+	        int initialMenuSize = restaurant.getMenu().size();
+	        restaurant.addToMenu("Lemon Chicken",280,true);
+	        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+	    }
+
+	    @Test
+	    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+	        mockData();
+	        int initialMenuSize = restaurant.getMenu().size();
+	        restaurant.removeFromMenu("Baida Curry");
+	        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+	    }
+
+	    @Test
+	    public void removing_item_that_does_not_exist_should_throw_exception() {
+	        mockData();
+	        assertThrows(itemNotFoundException.class,
+	                ()->restaurant.removeFromMenu("Jumbo Family Chicken Biryani"));
+	    }
+	    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
    
 }
